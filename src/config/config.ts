8550f9ba505c7ecb6ee4ded54a4a80a198e63635
Catherene <swismitha@yahoo.co.in>
2025-06-30 12:45:55 +0530

    Added config

--- src/config/config.ts
+++ src/config/config.ts
@@ -16,17 +16,17 @@ interface AppConfig {
 }
 
 const config: AppConfig = {
-    sgapikey : process.env.SENDGRID_API_KEY,
-    tomailid : process.env.TO_EMAIL,
-    frommailid : process.env.FROM_EMAIL,
+    sgapikey : process.env.SENDGRID_API_KEY || '',
+    tomailid : process.env.TO_EMAIL || '',
+    frommailid : process.env.FROM_EMAIL || '',
     awsregion: process.env.REGION || 'eu-north-1',
-    sqsURL: process.env.SQSURL,
+    sqsURL: process.env.SQSURL || '',
     azqueue: {
         queuename: process.env.AZQUEUE_NAME || 'js-queue-items',
         queueurl: 'AzureWebJobsStorage'
     },
-    vault: process.env.KEY_VAULT_URL,
-    clientID: process.env.CLIENT_ID
+    vault: process.env.KEY_VAULT_URL || '',
+    clientID: process.env.CLIENT_ID || ''
 };
 
 export default config;
